433a43b9efab9039b6857335aa7f726e6ddbaf6b
YouieDel <littlepostage@gmail.com>
2025-12-02 00:32:43 -0600

    pleease

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-website-2025/'
+  base: '/Portfolio-Website-2025/',
 })
